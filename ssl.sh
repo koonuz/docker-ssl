e73723ab68e35563ff6a764cb330792678df7457
koonuz <42229875+koonuz@users.noreply.github.com>
2023-03-13 20:21:22 +0800

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -211,7 +211,7 @@ check_crontab() {
 
 show_menu() {
     echo -e "
-  ${green}Acme.sh - 一键申请域名免费SSL证书${plain}
+  ${green}Acme.sh - 一键申请域名SSL证书${plain}
 ————————————————
   ${green}0.${plain} 退出脚本
 ————————————————
